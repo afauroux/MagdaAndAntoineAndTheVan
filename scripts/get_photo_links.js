a4351d4b1e9b6e54d17b23f7583380f03ab650b7
Antoine FAUROUX <antoine.fauroux@gmail.be>
2021-11-07 19:31:20 +0100

    perfectversion of the script

--- scripts/get_photo_links.js
+++ scripts/get_photo_links.js
@@ -1,12 +1,17 @@
 
 javascript: (function () {
-    let links = "Here are the links kochanie:\n\n";
+    let links = "";
     imgs = document.querySelectorAll('[data-latest-bg]');
     for (let a of imgs) {
         links+="![](" + a.style.backgroundImage.match(/http.*w/)[0] + "6000-h4000-no)\n"
     };
     console.log(links);
+
+    navigator.clipboard.writeText(links).then(function() {
+        /* clipboard successfully set */
+        alert("Links copied to clipboard successfully !");
+    }, function() {
+        /* clipboard write failed */
+        alert("Something weird occured !");
+    });
 })();
-
-
-
