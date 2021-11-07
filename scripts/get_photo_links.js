b1007de411d1fb7ed50afa12d48c44ee83870388
Antoine FAUROUX <antoine.fauroux@gmail.be>
2021-11-07 19:21:55 +0100

    hoops kochanie

--- scripts/get_photo_links.js
+++ scripts/get_photo_links.js
@@ -1,6 +1,6 @@
 
 javascript: (function () {
-    let links = "Here are the links kohanie:\n\n";
+    let links = "Here are the links kochanie:\n\n";
     imgs = document.querySelectorAll('[data-latest-bg]');
     for (let a of imgs) {
         links+="![](" + a.style.backgroundImage.match(/http.*w/)[0] + "6000-h4000-no)\n"
